bdff15e48c8e19bb0206b2d8c826f655d88d00c5
Abani-kumar <abanikumar073@gmail.com>
2024-06-19 16:59:00 +0530

    redis so many log bug fixed

--- src/client/redis/index.ts
+++ src/client/redis/index.ts
@@ -3,10 +3,4 @@ import { Redis } from "ioredis";
 
 export const redisClient = new Redis(process.env.REDIS_URL!);
 
-redisClient.on('connect', () => {
-  console.log('Connected to Redis');
-});
 
-redisClient.on('error', (err) => {
-  console.error('Redis error', err);
-});
