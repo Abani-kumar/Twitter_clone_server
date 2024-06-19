ff223a8e5d160fa9282f6f56e72679184af32d53
Abani-kumar <abanikumar073@gmail.com>
2024-06-19 19:34:20 +0530

    all redis issue solved final

--- src/client/redis/index.ts
+++ src/client/redis/index.ts
@@ -1,4 +1,4 @@
 import { Redis } from "ioredis";
 
-export const redisClient = new Redis("redis://default:AcENAAIncDFiOTA0N2I5ZDVkNWI0MDEwOGJlZGJjYzZkMjI3YTUzZnAxNDk0MjE@aware-catfish-49421.upstash.io:6379");
+export const redisClient = new Redis(process.env.REDIS_URL as string);
 
