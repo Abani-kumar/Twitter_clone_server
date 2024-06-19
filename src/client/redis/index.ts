966607f13d908aab7304d6e82cec39d2d0b122fd
Abani-kumar <abanikumar073@gmail.com>
2024-06-19 17:43:43 +0530

    error handle

--- src/client/redis/index.ts
+++ src/client/redis/index.ts
@@ -1,6 +1,25 @@
 import { Redis } from "ioredis";
 
+export const redisClient = new Redis(process.env.REDIS_URL!, {
+  retryStrategy(times) {
+    // Reconnect after
+    const delay = Math.min(times * 50, 2000);
+    return delay;
+  }
+});
 
-export const redisClient = new Redis(process.env.REDIS_URL!);
+redisClient.on('connect', () => {
+  console.log('Connected to Redis');
+});
 
+redisClient.on('reconnecting', () => {
+  console.log(`Reconnecting to Redis ...`);
+});
 
+redisClient.on('end', () => {
+  console.log('Redis connection closed');
+});
+
+redisClient.on('error', (err) => {
+  console.error('Redis error', err);
+});
