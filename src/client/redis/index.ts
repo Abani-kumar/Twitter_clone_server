38a70df17623d33f82bef89f4e6b6886a1f92dfc
Abani-kumar <abanikumar073@gmail.com>
2024-06-19 16:45:32 +0530

    Redis commit bug fixed

--- src/client/redis/index.ts
+++ src/client/redis/index.ts
@@ -1,5 +1,12 @@
-import Redis from "ioredis";
+import { Redis } from "ioredis";
 
-export const redisClient = new Redis(
-  `redis://default:${process.env.REDIS_PASSWORD}@${process.env.REDIS_ENDPOINT}:${process.env.REDIS_PORT}`
-);
+
+export const redisClient = new Redis(process.env.REDIS_URL!);
+
+redisClient.on('connect', () => {
+  console.log('Connected to Redis');
+});
+
+redisClient.on('error', (err) => {
+  console.error('Redis error', err);
+});
